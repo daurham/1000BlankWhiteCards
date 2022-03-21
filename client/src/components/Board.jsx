523e07cbbc17829f20133f084e63aacd8d8b28eb
jinicha <jinichaa@gmail.com>
2022-03-21 11:38:10 -0700

    Create TurnTracker.jsx

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
+import TurnTracker from './TurnTracker.jsx';
 
 const Container = styled.div`
   display: flex;
@@ -59,6 +60,7 @@ export default function GameBoard() {
           </Innerboard>
         </Board>
       </Background>
+      <TurnTracker />
     </Container>
   );
 };
