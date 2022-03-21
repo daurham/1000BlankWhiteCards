7138abc97df43a9f351f481df10981b643b6785c
Karli Thomas <karliholthouse@gmail.com>
2022-03-21 15:52:28 -0700

    pulling recent changes

--- client/src/components/DrawingArea.jsx
+++ client/src/components/DrawingArea.jsx
@@ -1,76 +1,76 @@
-// import React from 'react';
-// import { Stage, Layer, Line } from 'react-konva';
-// import { useEffect, useState, useRef } from 'react';
+import React from 'react';
+import { Stage, Layer, Line } from 'react-konva';
+import { useEffect, useState, useRef } from 'react';
 
-// const DrawingArea = ({onClearLines, clearLines}) => {
+const DrawingArea = ({onClearLines, clearLines}) => {
 
-//     const [lines, setLines] = useState([]);
-//     const isDrawing = useRef(false);
+    const [lines, setLines] = useState([]);
+    const isDrawing = useRef(false);
 
-//     useEffect(() => {
-//         //loadImage();
-//     }, [clearLines])
+    useEffect(() => {
+        //loadImage();
+    }, [clearLines])
 
-//     const handleMouseDown = (e) => {
-//         isDrawing.current = true;
-//         const pos = e.target.getStage().getPointerPosition();
-//         setLines([...lines, { points: [pos.x, pos.y] }]);
-//     };
+    const handleMouseDown = (e) => {
+        isDrawing.current = true;
+        const pos = e.target.getStage().getPointerPosition();
+        setLines([...lines, { points: [pos.x, pos.y] }]);
+    };
 
-//     const handleMouseMove = (e) => {
-//         // no drawing - skipping
-//         if (!isDrawing.current) {
-//           return;
-//         }
-//         const stage = e.target.getStage();
-//         const point = stage.getPointerPosition();
+    const handleMouseMove = (e) => {
+        // no drawing - skipping
+        if (!isDrawing.current) {
+          return;
+        }
+        const stage = e.target.getStage();
+        const point = stage.getPointerPosition();
 
-//         // To draw line
-//         let lastLine = lines[lines.length - 1];
+        // To draw line
+        let lastLine = lines[lines.length - 1];
 
-//         if(lastLine) {
-//             // add point
-//             lastLine.points = lastLine.points.concat([point.x, point.y]);
+        if(lastLine) {
+            // add point
+            lastLine.points = lastLine.points.concat([point.x, point.y]);
 
-//             // replace last
-//             lines.splice(lines.length - 1, 1, lastLine);
-//             setLines(lines.concat());
-//         }
+            // replace last
+            lines.splice(lines.length - 1, 1, lastLine);
+            setLines(lines.concat());
+        }
 
-//     };
+    };
 
-//     const handleMouseUp = () => {
-//         isDrawing.current = false;
-//     };
+    const handleMouseUp = () => {
+        isDrawing.current = false;
+    };
 
-//     return (
-//         <div className=" text-center text-dark">
-//             <Stage
-//                 width={600}
-//                 height={600}
-//                 onMouseDown={handleMouseDown}
-//                 onMousemove={handleMouseMove}
-//                 onMouseup={handleMouseUp}
-//                 className="canvas-stage"
-//             >
-//                 <Layer>
-//                     {lines.map((line, i) => (
-//                         <Line
-//                         key={i}
-//                         points={line.points}
-//                         stroke="#df4b26"
-//                         strokeWidth={2}
-//                         tension={0.5}
-//                         lineCap="round"
-//                         globalCompositeOperation={
-//                             line.tool === 'eraser' ? 'destination-out' : 'source-over'
-//                         }
-//                         />
-//                     ))}
-//                 </Layer>
-//             </Stage>
-//         </div>
-//     )
-// }
+    return (
+        <div className=" text-center text-dark">
+            <Stage
+                width={600}
+                height={600}
+                onMouseDown={handleMouseDown}
+                onMousemove={handleMouseMove}
+                onMouseup={handleMouseUp}
+                className="canvas-stage"
+            >
+                <Layer>
+                    {lines.map((line, i) => (
+                        <Line
+                        key={i}
+                        points={line.points}
+                        stroke="#df4b26"
+                        strokeWidth={2}
+                        tension={0.5}
+                        lineCap="round"
+                        globalCompositeOperation={
+                            line.tool === 'eraser' ? 'destination-out' : 'source-over'
+                        }
+                        />
+                    ))}
+                </Layer>
+            </Stage>
+        </div>
+    )
+}
 
-// export default DrawingArea;
+export default DrawingArea;
--- client/src/components/Lobby.jsx
+++ client/src/components/Lobby.jsx
@@ -1,16 +1,122 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import Home from './Home.jsx'
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import Home from './Home.jsx'
 // import DrawingArea from './DrawingArea';
+// import CanvasDraw from "react-canvas-draw";
+// import Button from '@mui/material/Button';
+// import Modal from '@mui/material/Modal';
 
 
-export default function Lobby() {
+// export default function Lobby() {
+//   const [open, setOpen] = useState(false);
+//   const handleOpen = () => setOpen(true);
+//   const handleClose = () => setOpen(false);
+//   const [showCanvas, setShowCanvas] = useState(false)
+
+//   // const handleClick = function () {
+//   //   setShowCanvas(!showCanvas);
+//   // }
+
+//   return (
+//     <div>
+//       <h1>Lobby</h1>
+//       <div>
+//         <Button onClick={handleOpen} varaint="contained" color="primary">Add a Card</Button>
+//         {/* <DrawingArea /> */}
+
+
+//         {/* {showCanvas ? */}
+//           <Modal
+//             open={open}
+//             onClose={handleClose}
+//             // aria-labelledby="modal-modal-title"
+//             // aria-describedby="modal-modal-description"
+//           >
+//             <CanvasDraw />
+//           </Modal>
+//           {/* : null} */}
+//       </div>
+//       <Link to="/Game">Go to Game!</Link>
+//       {/* <br /> */}
+//       <Link to="/">Exit</Link>
+//     </div>
+//   );
+// }
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Button, Typography, Modal} from '@mui/material';
+import CanvasDraw from "react-canvas-draw";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function BasicModal() {
+  const [openCanvas, setCanvasOpen] = useState(false);
+  const handleCanvasOpen = () => setCanvasOpen(true);
+  const handleCanvasClose = () => setCanvasOpen(false);
+  const [selectedColor, setSelectedColor] = useState('red');
+
+  const [openDeck, setDeckOpen] = useState(false);
+  const handleDeckOpen = () => setDeckOpen(true);
+  const handleDeckClose = () => setDeckOpen(false);
 
   return (
-  <div>
-    <h1>Lobby</h1>
-    <Link to="/Game">Go to Game!</Link>
-    <Link to="/">Exit</Link>
-  </div>
+    <div>
+      <h1>Lobby</h1>
+      <div>
+        <h4>Players</h4>
+        <div>Player 1</div>
+        <div>Player 2</div>
+        <div>Player 3</div>
+        <div>Player 4</div>
+      </div>
+      <Button onClick={handleCanvasOpen}>Add A Card!</Button>
+      <Modal
+        open={openCanvas}
+        onClose={handleCanvasClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+            Draw Below!:
+          </Typography>
+          <CanvasDraw brushColor={selectedColor}/>
+        </Box>
+      </Modal>
+
+      <div>
+      <div>
+      <Button onClick={handleDeckOpen}>Edit Deck</Button>
+      <Modal
+        open={openDeck}
+        onClose={handleDeckClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Library || Current Deck
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+
+          </Typography>
+        </Box>
+      </Modal>
+      </div>
+        <Link to="/Game">START GAME</Link>
+        <br />
+        <Link to="/">Exit</Link>
+      </div>
+    </div>
   );
 }
