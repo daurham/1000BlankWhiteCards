223c55b03695ab08665ce0aad1d5da68d954488f
Karli Thomas <karliholthouse@gmail.com>
2022-03-24 11:45:35 -0700

    added lobby and username constraints

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import MediaCard from './Card.jsx';
 import { useData } from '../UseContext';
 
+const sortFunctions = {
+  newest: (a, b) => (new Date(a.dateCreated)) > (new Date(b.dateCreated)) ? -1 : 1,
+  oldest: (a, b) => (new Date(a.dateCreated)) < (new Date(b.dateCreated)) ? -1 : 1,
+  createdby: (a, b) => a.createdBy < b.createdBy ? -1 : 1,
+}
+
 export default function Cards() {
-  const { cards } = useData();
+  const { cards, sort } = useData();
+  const sorted = [...cards];
+  sorted.sort(sortFunctions[sort]);
   return (
-      cards.map((card, key) => <MediaCard card={card} key={key} />)
+      sorted.map((card, key) => <MediaCard card={card} key={key} />)
   )
 };
