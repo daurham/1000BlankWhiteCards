6c7b4631b3e4a4680bb62d15c557e7818d44b9c0
jinicha <jinichaa@gmail.com>
2022-03-25 09:19:26 -0700

    Add css

--- client/src/components/Library.jsx
+++ client/src/components/Library.jsx
@@ -3,6 +3,9 @@ import {Link} from 'react-router-dom';
 import Home from './Home.jsx';
 import Cards from './Cards.jsx';
 import { useData } from '../UseContext';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 export default function Library() {
   const { sort, setSort } = useData();
@@ -21,17 +24,20 @@ export default function Library() {
 
   return (
     <div>
-    <h1>Library</h1>
-    <select name='cardsort' id='cardsort' onChange={handleSort}>
-      <option value='oldest' >Oldest</option>
-      <option value='newest' >Newest</option>
-      <option value='createdby' >Created By</option>
-    </select>
-    <div style={container}>
-      <Cards />
-    </div>
-
-    <Link to="/">Back to home</Link>
+      <Typography variant='h1' id='libraryitle'>Library</Typography>
+      <Typography >
+        <Stack spacing={5} direction='row' className='library'>
+          <select name='cardsort' id='cardsort' onChange={handleSort}>
+            <option value='oldest' >Oldest</option>
+            <option value='newest' >Newest</option>
+            <option value='createdby' >Created By</option>
+          </select>
+          <Button variant='outlined' size='large' id='lib-btn1' href="/">Back to home</Button>
+        </Stack>
+      </Typography>
+      <div style={container} className='library'>
+        <Cards />
+      </div>
     </div>
   )
 }
